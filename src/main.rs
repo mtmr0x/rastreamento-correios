bc37015bc4fd3ad2cbdce6b09409aa20ed4ac456
Matheus Marsiglio <matmarsiglio@gmail.com>
2017-05-03 21:54:47 -0300

    add: first main function with base route all

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+#[macro_use] extern crate nickel;
+
+use nickel::{Nickkel, HttpRouter};
+
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+    server.get('*', middleware!("Hello Fucking World"));
+    server.listen("127.0.0.1:6767");
 }
+
