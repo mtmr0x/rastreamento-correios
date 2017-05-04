d74c9ed293906f0c9820f63febea60733f92f8c3
Matheus Marsiglio <matmarsiglio@gmail.com>
2017-05-03 21:59:23 -0300

    add: only homepage returns hello world

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use nickel::{Nickel, HttpRouter};
 
 fn main() {
     let mut server = Nickel::new();
-    server.get("**", middleware!("Hello Fucking World"));
+    server.get("/", middleware!("Hello Fucking World"));
     server.listen("127.0.0.1:6767");
 }
 
