81a3d36228dafa5f7dfee68415e3153eb7f7090b
Matheus Marsiglio <matmarsiglio@gmail.com>
2017-05-03 21:58:06 -0300

    fix: typo classes call

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 #[macro_use] extern crate nickel;
 
-use nickel::{Nickkel, HttpRouter};
+use nickel::{Nickel, HttpRouter};
 
 fn main() {
     let mut server = Nickel::new();
-    server.get('*', middleware!("Hello Fucking World"));
+    server.get("**", middleware!("Hello Fucking World"));
     server.listen("127.0.0.1:6767");
 }
 
